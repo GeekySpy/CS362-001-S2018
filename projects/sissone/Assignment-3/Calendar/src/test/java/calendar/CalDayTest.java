65ad20f89b0509a47ccc09ae2be61e83714583cd
Eric Sisson <sissone@flip3.engr.oregonstate.edu>
2018-05-07 20:44:11 -0700

    Added Assignment 3

---
+++ projects/sissone/Assignment-3/Calendar/src/test/java/calendar/CalDayTest.java
@@ -0,0 +1,38 @@
+/** A JUnit test class to test the class CalDay. */
+
+package calendar;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import calendar.Appt;
+import calendar.CalDay;
+
+
+public class CalDayTest{
+  //Testing the iterator function
+  //Not working
+  @Test(timeout = 4000)
+  public void test00()  throws Throwable  {
+  /*  Appt appt0 = new Appt(15, 30, 9, 14, 2018, "Birthday Party", "This is my birthday party", "xyz@gmail.com");
+    //Invalid appt
+    Appt appt1 = new Appt(30, 30, 9, 14, 2018, "Birthday Party", "This is my birthday party", "xyz@gmail.com");
+    //First appt, 0th hour
+    Appt appt3 = new Appt(0, 30, 9, 14, 2018, "Birthday Party", "This is my birthday party", "xyz@gmail.com");
+    CalDay null_day = new CalDay();
+    //GregorianCalendar derp = new GregorianCalendar (2018, 4, 23);
+    //CalDay calday = new CalDay(derp);
+    calday.addAppt(appt0);
+    //calday.addAppt(appt2);
+    //calday.addAppt(appt1);
+    //Iterator null_appt = null_day.iterator();
+    //Iterator appt = calday.iterator();
+    //assertEquals(null, null_appt);
+    //assertNotEquals(null, appt);*/
+  }
+  //Testing the addAppt function
+  @Test(timeout = 4000)
+  public void test01()  throws Throwable  {
+
+  }
+
+}
